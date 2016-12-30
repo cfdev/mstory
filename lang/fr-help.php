8ac29d6a0419f1f04a4f155e1f8671e90f108f43
Cyril <cfdev161@gmail.com>
2016-12-30 18:47:05 +0100

    Lang

---
+++ lang/fr-help.php
@@ -0,0 +1,33 @@
+<?php if (!defined('PLX_ROOT')) exit; ?>
+
+<p class="alert green">Soutenez mon travail en <strong><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=8FYFQTCFQ9WZN">faisant un Don!</a></strong> Merci beaucoup.</p>
+
+<h3>Thème compatible PuXml version 5.5</h3>
+<ul>
+  <li>Affichage des images d'accroches pour les articles</li>
+</ul>
+
+
+<h3>Section dans la page d'accueil</h3>
+<p>Le thème utilise la page statique numéro 1 dans la section au dessus des articles.</p>
+
+<h3>Les liens des réseaux sociaux</h3>
+<p>Pour insérer vos liens vers les réseaux sociaux, il vous faut éditer le fichier footer.php dans le thème. En modifiant le '#' :</p>
+<p>&lt;a href="#"&gt;&lt;i class="fa fa-twitter fa-fw fa-2x"&gt; &lt;/i&gt;&lt;br&gt;Twitter&lt;/a&gt;</p>
+<p>&nbsp;</p>
+
+<h3>Utilisation de shortcodes pour PluCSS</h3>
+<p>	Shortcodes plugin: <a href="http://secretsitebox.fr/site/index.php?categorie2/pluxml-plugins#post-10/">Téléchargement</a>. <p>
+<pre>[info]informations[/info]</pre>
+<pre>[success]Super![/success]</pre>
+<pre>[warning]Important![/warning]</pre>
+<pre>[error]ERReuR 404[/error]</pre>
+<pre>[hand]A voir absolument[/hand]</pre>
+<pre>[clock]Disponible ...[/clock]</pre>
+<pre>[euro] prix...[/euro]</pre>
+<pre>[button color="green" link="http://pluxml.org"]PluXml Power[/button]</pre>
+<pre>[progressbar val="50%"]</pre>
+
+
+<h3>Conditions d'utilisations du thème</h3>
+<p>La mention faisant référence à l'auteur dans le <i>footer.php</i> est <strong>obligatoire</strong> pour utiliser ce thème (GPL).<br/>Merci de ne pas la supprimer...</p>
